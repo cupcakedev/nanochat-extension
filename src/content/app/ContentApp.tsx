0562006d62aaee748fb89bc76e68b201769055ea
lenadweb <lenadweber@gmail.com>
2026-05-05 22:03:49 +0700

    rename folder

--- src/content/app/ContentApp.tsx
+++ src/content/app/ContentApp.tsx
@@ -1,4 +1,4 @@
-import { SelectionPopupOverlay } from '@content/services/text-selection-react/SelectionPopupOverlay';
+import { SelectionPopupOverlay } from '@content/services/context-menu/SelectionPopupOverlay';
 import { useRuntimeMessageBridge } from './useRuntimeMessageBridge';
 
 export function ContentApp() {
